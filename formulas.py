2d78bfde04238f82391d05d2a397d1b2a9351efc
lachlanmonty <montylachlan@gmail.com>
2022-06-16 16:56:30 +1000

    updated

--- formulas.py
+++ formulas.py
@@ -1,8 +1,8 @@
 import math
 
+
 def bingham_darcy_friction_factor(d_m, v, rho, t_0, mu_b):
-    
-    r"""Calculates the Darcy friction factor for a Bingham plastic.
+    """Calculates the Darcy friction factor for a Bingham plastic.
     Calculated for any Reynolds number, from laminar through turbulent. [1]
        
     Parameters
@@ -34,33 +34,37 @@ def bingham_darcy_friction_factor(d_m, v, rho, t_0, mu_b):
 
     """
 
-    N_re = d_m*v*rho/mu_b
+    N_re = d_m * v * rho / mu_b
 
-    N_he = d_m**2*rho*t_0/mu_b**2
+    N_he = d_m ** 2 * rho * t_0 / mu_b ** 2
 
     # Laminar
-    f_L = 16/N_re*(1+1/6*(N_he/N_re))
-    
+    f_L = 16 / N_re * (1 + 1 / 6 * (N_he / N_re))
+
     for i in range(10):
-        f_L = 16/N_re*(1+1/6*(N_he/N_re)-((N_he**4)/(3*f_L**3*N_re**7)))  
-        
-    f_L = 4 * f_L       # Converts to Darcy Friction Factor
-    
+        f_L = (
+            16
+            / N_re
+            * (1 + 1 / 6 * (N_he / N_re) - ((N_he ** 4) / (3 * f_L ** 3 * N_re ** 7)))
+        )
+
+    f_L = 4 * f_L  # Converts to Darcy Friction Factor
+
     # Turbulent
-    a = -1.47*(1+0.146*math.exp(-2.9*10**-5*N_he))
-    f_T = 4 * (10**a/N_re**0.193)
-    
+    a = -1.47 * (1 + 0.146 * math.exp(-2.9 * 10 ** -5 * N_he))
+    f_T = 4 * (10 ** a / N_re ** 0.193)
+
     # Combine
-    m = 1.7+40000/N_re
-        
-    f_d = (f_L**m+f_T**m)**(1/m)
+    m = 1.7 + 40000 / N_re
+
+    f_d = (f_L ** m + f_T ** m) ** (1 / m)
 
     return f_d
 
 
-def oroskar_settling_velocity(D, d, S, C_c, p_f, u_f, X = 1, g = 9.81):
-    
-    r"""Calculates the settling or critical velocity for a slurry with a 
+def oroskar_settling_velocity(D, d, S, C_c, p_f, u_f, X=1, g=9.81):
+
+    """Calculates the settling or critical velocity for a slurry with a 
     significant portion of fines and coarse particles. [1]
        
     Parameters
@@ -99,29 +103,37 @@ def oroskar_settling_velocity(D, d, S, C_c, p_f, u_f, X = 1, g = 9.81):
     https://www.pnnl.gov/rpp-wtp/documents/WTP-RPT-175.pdf
 
     """
-        
-    V_ot = ((g*d*(S-1))**0.5)*\
-    (\
-        (1.85*C_c**0.1536)*\
-        ((1-C_c)**0.3567)*\
-        ((D/d)**0.378)*\
-        (((p_f*D*((g*d*(S-1))**0.5))/u_f)**0.09)*\
-        (X**0.3)
+
+    V_ot = ((g * d * (S - 1)) ** 0.5) * (
+        (1.85 * C_c ** 0.1536)
+        * ((1 - C_c) ** 0.3567)
+        * ((D / d) ** 0.378)
+        * (((p_f * D * ((g * d * (S - 1)) ** 0.5)) / u_f) ** 0.09)
+        * (X ** 0.3)
     )
     return V_ot
 
 
-def pump_power(q_m3h, rho, head, efficiency, g = 9.81):
+def pump_power(q_m3h, rho, head, efficiency, g=9.81):
+    """Pump Power
 
-    P_kw = round((((q_m3h/60/60)* rho * g * head)/ efficiency)/1000,2)
+    Args:
+        q_m3h (float): Flowrate [m3/h]
+        rho (float): Density [kg/m3]
+        head (float): Head [m]
+        efficiency (float): Pump efficiency [%]
+        g (float, optional): Gravitational acceleration. Defaults to 9.81.
+
+    Returns:
+        power: pump power [kW]
+    """
+    P_kw = round((((q_m3h / 60 / 60) * rho * g * head) / efficiency) / 1000, 2)
 
     return P_kw
 
 
-
 def api_standard_520_part1_sizing(d_m, v, rho, t_0, mu_b):
-    
-    r"""Calculates required size for rupture disc with liquid medium. [1]
+    """Calculates required size for rupture disc with liquid medium. [1]
        
     Parameters
     ----------
@@ -156,7 +168,7 @@ def api_standard_520_part1_sizing(d_m, v, rho, t_0, mu_b):
 
 
 def cema_area(BW, beta, phi):
-    r"""CEMA Standard Capacity Cross Sectional Area, $A_s$
+    """CEMA Standard Capacity Cross Sectional Area, $A_s$
 
     Equation 4.15 is used to calculate, As, for standard CEMA three equal roll 
     roughing idlers based on the average CEMA center roll length circular 
@@ -179,26 +191,28 @@ def cema_area(BW, beta, phi):
         and standard edge distance, [m²]
 
     """
-    beta =  math.radians(beta)
-    phi  =  math.radians(phi)
+    beta = math.radians(beta)
+    phi = math.radians(phi)
 
-    bc = (0.371 * BW + 6.35)/BW         # Equation 4.11
-    bw = (1-bc)/2                       # Equation 4.13
-    bwe = (0.055 * BW + 22.9)/BW        # Equation 4.12
+    bc = (0.371 * BW + 6.35) / BW  # Equation 4.11
+    bw = (1 - bc) / 2  # Equation 4.13
+    bwe = (0.055 * BW + 22.9) / BW  # Equation 4.12
     bwmc = bw - bwe
-    rsch = ((bc/2)/(math.sin(phi)))+((math.cos(beta)*bwmc)/(math.sin(phi)))
+    rsch = ((bc / 2) / (math.sin(phi))) + ((math.cos(beta) * bwmc) / (math.sin(phi)))
 
-    A_s = 2*BW**2 * \
-        (rsch**2*((phi/2)-((math.sin(phi)*math.cos(phi))/2)))+\
-        ((bc/2) * bwmc * math.sin(beta)) + \
-        (bwmc**2 * ((math.sin(beta) * math.cos(beta))/2))
+    A_s = (
+        2 * BW ** 2 * (rsch ** 2 * ((phi / 2) - ((math.sin(phi) * math.cos(phi)) / 2)))
+        + ((bc / 2) * bwmc * math.sin(beta))
+        + (bwmc ** 2 * ((math.sin(beta) * math.cos(beta)) / 2))
+    )
 
-    A_s = A_s / 1000**2                 # Convert to m²
+    A_s = A_s / 1000 ** 2  # Convert to m²
 
     return A_s
 
+
 def req_conv_area(bulk_density, tonnage, belt_velocity):
-    r"""Required Area for Mass Flow
+    """Required Area for Mass Flow
 
     The conveyed bulk material cross sectional area, A, can be calculated from
     the design inputs for tonnage, Q, belt speed, V, bulk density, $\gamma_m$,
@@ -222,4 +236,5 @@ def req_conv_area(bulk_density, tonnage, belt_velocity):
     - $A$ : float
         - Required conveyed cross sectional area, [m²]
     """
-    return (tonnage * 1000/60/60) / (belt_velocity * bulk_density)
+    return (tonnage * 1000 / 60 / 60) / (belt_velocity * bulk_density)
+
